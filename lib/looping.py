5fa742ff21a9a36915b030afbedc7f7b16449872
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-05-20 21:06:36 +0300

    square and happy_new_year

--- lib/looping.py
+++ lib/looping.py
@@ -2,11 +2,16 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    countdown = 11
+    while countdown > 0:
+        print(f"{countdown}")
+        countdown -= 1
+        if countdown == 0:
+            print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [i ** 2 for i in int_list]
 
 def fizzbuzz():
     # code goes here!

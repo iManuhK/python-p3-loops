8104dd39863f2895e50886e5d59b1160a4861202
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-05-20 21:08:10 +0300

    adds the fizzbuzz

--- lib/looping.py
+++ lib/looping.py
@@ -15,4 +15,12 @@ def square_integers(int_list):
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
